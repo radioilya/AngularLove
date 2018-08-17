3523005a77ea603279ad6709c76a593c67621dd1
radioilya <radioilya@yandex.ru>
2018-08-17 16:31:31 +0700

    Правки и учим дальше

--- tasks 3/src/app/app.module.ts
+++ tasks 3/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopmenuComponent } from './header/topmenu/topmenu.component';
-import { ContetnZarezervirovannoComponent } from './contetn-zarezervirovanno/contetn-zarezervirovanno.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { MediaCssComponent } from './media-css/media-css.component';
--- tasks 3/src/app/main/main.component.ts
+++ tasks 3/src/app/main/main.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PageH1 } from '../no_name';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
+
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  pageH1: PageH1 = {
+    id: 10,
+    name: 'Обо мне'
+  };
+  constructor() {
+
+
+    }
 
   ngOnInit() {
   }
